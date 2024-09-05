c87ff8a9d5d6fca00279110db504e10c76f15fab
anaxkyamauchi <k.yamauchi@ana-x.co.jp>
2024-09-05 14:47:28 +0900

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,103 +1,38 @@
-import {useState} from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
 
-import {Authenticator} from '@aws-amplify/ui-react'
-import {InvokeCommand, InvokeWithResponseStreamCommand, LambdaClient} from '@aws-sdk/client-lambda'
-import {fetchAuthSession} from 'aws-amplify/auth'
+import { Header } from './components/section/Header.tsx';
+import { MainContent } from './components/section/MainContent.tsx';
 
-import outputs from "../amplify_outputs.json"
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1565C0', // 深い青（飛行機や空をイメージ）
+        },
+        secondary: {
+            main: '#FFA726', // オレンジ（旅行や太陽をイメージ）
+        },
+        background: {
+            default: '#E3F2FD', // 薄い青（雲や空をイメージ）
+        },
+    },
+});
 
-
-function App() {
-    const [text, setText] = useState("")
-    const [prompt, setPrompt] = useState("")
-    const [aiMessage, setAiMessage] = useState("")
-
-    async function invokeHelloWorld() {
-
-        const {credentials} = await fetchAuthSession()
-        const awsRegion = outputs.auth.aws_region
-        const functionName = outputs.custom.helloWorldFunctionName
-
-        const labmda = new LambdaClient({credentials: credentials, region: awsRegion})
-        const command = new InvokeCommand({
-            FunctionName: functionName,
-        });
-        const apiResponse = await labmda.send(command);
-
-        if (apiResponse.Payload) {
-            const payload = JSON.parse(new TextDecoder().decode(apiResponse.Payload))
-            setText(payload.message)
-        }
-    }
-
-    async function invokeBedrock() {
-
-        const {credentials} = await fetchAuthSession()
-        const awsRegion = outputs.auth.aws_region
-        const functionName = outputs.custom.invokeBedrockFunctionName
-
-        const labmda = new LambdaClient({credentials: credentials, region: awsRegion})
-        const command = new InvokeWithResponseStreamCommand({
-            FunctionName: functionName,
-            Payload: new TextEncoder().encode(JSON.stringify({prompt: prompt}))
-        })
-        const apiResponse = await labmda.send(command);
-
-        let completeMessage = ''
-        if (apiResponse.EventStream) {
-            for await (const item of apiResponse.EventStream) {
-                if (item.PayloadChunk) {
-                    const payload = new TextDecoder().decode(item.PayloadChunk.Payload)
-                    completeMessage = completeMessage + payload
-                    setAiMessage(completeMessage)
-                }
-            }
-        }
-    }
-
-    return (
-        <Authenticator>
-            {({signOut, user}) => (
-                <>
-                    <div>
-                        <a href="https://docs.amplify.aws" target="_blank">
-                            <img src="https://docs.amplify.aws/assets/icon/favicon-purple-96x96.png"
-                                 className="logo amplify" alt="Amplify logo"/>
-                        </a>
-                        <a href="https://vitejs.dev" target="_blank">
-                            <img src={viteLogo} className="logo" alt="Vite logo"/>
-                        </a>
-                        <a href="https://react.dev" target="_blank">
-                            <img src={reactLogo} className="logo react" alt="React logo"/>
-                        </a>
-                    </div>
-                    <h1>Amplify + Vite + React</h1>
-                    <p>
-                        Hello, {user?.signInDetails?.loginId}
-                        <br/>
-                        <button onClick={signOut}>Sign Out</button>
-                    </p>
-                    <p>
-                        <button onClick={invokeHelloWorld}>invokeHelloWorld</button>
-                        <div>{text}</div>
-                    </p>
-                    <p>
-            <textarea
-                onChange={(e) => setPrompt(e.target.value)}
-                value={prompt}
-                style={{width: '50vw', textAlign: 'left'}}
-            ></textarea>
-                        <br/>
-                        <button onClick={invokeBedrock}>invokeBedrock</button>
-                        <div style={{width: '50vw', textAlign: 'left'}}>{aiMessage}</div>
-                    </p>
-                </>
-            )}
-        </Authenticator>
-    )
+const boxStyle = {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
 }
 
-export default App
+export default function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <Box sx={boxStyle}>
+                <Header/>
+                <MainContent/>
+            </Box>
+        </ThemeProvider>
+    )
+}
